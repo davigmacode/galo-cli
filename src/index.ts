ff4313d0ee0561a4fde72557dec5ce7328c58e56
Irfan Vigma Taufik <davigmacode@gmail.com>
2021-10-23 07:36:04 +0700

    Improve log time, shebang to make cli work, and change command name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Command } from "commander";
 import { LIB_NAME, LIB_VERSION } from "./constants";
 import { cwd } from "./helpers/file";
@@ -13,17 +15,29 @@ import cleanAction from "./actions/clean";
 import chalk from "chalk";
 import * as header from "./header";
 
+const banner = chalk.green(header.logo) + ' ' + chalk.bgBlue(header.author);
 const program = new Command();
 
 program
   .name(LIB_NAME)
   .version(LIB_VERSION)
-  .description(chalk.green(header.logo) + ' ' + chalk.bgBlue(header.author))
-  .showHelpAfterError();
+  .addHelpText("beforeAll", banner)
+  .showHelpAfterError()
+  .hook("preAction", (_, act) => {
+    console.log('');
+    console.log(act.description().toUpperCase());
+    console.log('===========');
+    console.log(`${LIB_NAME} v${LIB_VERSION}`);
+    console.log('');
+    console.time(act.name());
+  })
+  .hook("postAction", (_, act) => {
+    console.timeEnd(act.name());
+  });
 
 program
   .command('init')
-  .description('create a new generative artworks collection')
+  .description('initialize a new generative artworks collection')
   .argument(
     '[dir]',
     'directory to init the artworks collection',
@@ -36,9 +50,7 @@ program
   .option('-tp, --traits-path <path>', 'set traits path', './traits')
   .option('-mp, --metadata-path <path>', 'set generated metadata path', './metadata')
   .option('-ap, --artworks-path <path>', 'set generated artworks path', './artworks')
-  .option('-sp, --storage-provider <name>', 'set storage provider name', 'NFTStorage')
-  .option('-sk, --storage-key <token>', 'set storage key token')
-  .option('-p, --preview <path>', 'set generated collage preview path', './collage.png')
+  .option('-p, --preview-path <path>', 'set generated collage preview path', './collage.png')
   .action(initAction);
 
 program
@@ -49,43 +61,43 @@ program
     (val) => cwd(val),
     process.cwd()
   )
-  .description('generate the artworks, metadata, rarity, and collage')
+  .description('build the artworks, metadata, rarity, and collage')
   .option('-c, --config <path>', 'set config path', './config.json')
   .action(buildAction);
 
 program
-  .command('build:metadata')
+  .command('metadata')
   .argument(
     '[dir]',
     'collection directory, use current dir if not supplied',
     (val) => cwd(val),
     process.cwd()
   )
-  .description('create collection metadata without regenerating the collection')
+  .description('create collection metadata without regenerating the artworks')
   .option('-c, --config <path>', 'set config path', './config.json')
   .action(metadataAction);
 
 program
-  .command('build:collage')
+  .command('collage')
   .argument(
     '[dir]',
     'collection directory, use current dir if not supplied',
     (val) => cwd(val),
     process.cwd()
   )
-  .description('create collection collage without regenerating the collection')
+  .description('create collection collage without regenerating the artworks')
   .option('-c, --config <path>', 'set config path', './config.json')
   .action(collageAction);
 
 program
-  .command('build:rarity')
+  .command('rarity')
   .argument(
     '[dir]',
     'collection directory, use current dir if not supplied',
     (val) => cwd(val),
     process.cwd()
   )
-  .description('create collection rarity without regenerating the collection')
+  .description('create collection rarity without regenerating the artworks')
   .option('-c, --config <path>', 'set config path', './config.json')
   .action(rarityAction);
 
@@ -97,7 +109,7 @@ program
     (val) => cwd(val),
     process.cwd()
   )
-  .description('upload the generated artworks to decentralized storage')
+  .description('upload the artworks and metadata to decentralized storage')
   .option('-c, --config <path>', 'set config path', './config.json')
   .action(uploadAction);
 
