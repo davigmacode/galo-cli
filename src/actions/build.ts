7d6ddb164e51ce3d49d30c5ddae7d0983227a76f
Galokeun Artwork <galokeun.art@gmail.com>
2021-11-09 19:29:21 +0700

    Attach rarity to each gen attributes

--- src/actions/build.ts
+++ src/actions/build.ts
@@ -137,6 +137,41 @@ export default async (basePath: string, opt: any) => {
 
   if (isNil(generations) || isEmpty(generations)) return;
 
+  const metadataConfig = pathJoin(basePath, config.metadata.config);
+  const collagePath = pathJoin(basePath, config.collage.name);
+  const rarityJson = pathJoin(basePath, 'rarity.json');
+  const rarityCsv = pathJoin(basePath, 'rarity.csv');
+
+  await task({
+    processText: 'Removing previously generated content',
+    successText: `Removed previously generated content`,
+    fn: async () => {
+      deleteFile(metadataConfig, '.json');
+      deleteFile(collagePath, '.png');
+      deleteFile(rarityJson, '.json');
+      deleteFile(rarityCsv, '.csv');
+    }
+  });
+
+  // populating rarity
+  const rarity = await task({
+    processText: 'Populating rarity',
+    successText: `Collection Rarity is ready`,
+    fn: async () => populateRarity(traits, generations),
+  });
+
+  await task({
+    processText: 'Writing rarity to .json',
+    successText: `Collection Rarity: ${rarityJson}`,
+    fn: async () => writeJson(rarityJson, rarity),
+  });
+
+  await task({
+    processText: 'Writing rarity to .csv',
+    successText: `Collection Rarity: ${rarityCsv}`,
+    fn: async () => rarityToCSV(rarityCsv, rarity),
+  });
+
   // ensure artworks directory
   const artworksPath = pathJoin(basePath, config.artworks.path);
   await task({
@@ -159,29 +194,13 @@ export default async (basePath: string, opt: any) => {
     }
   });
 
-  const metadataConfig = pathJoin(basePath, config.metadata.config);
-  const collagePath = pathJoin(basePath, config.collage.name);
-  const rarityJson = pathJoin(basePath, 'rarity.json');
-  const rarityCsv = pathJoin(basePath, 'rarity.csv');
-
-  await task({
-    processText: 'Removing previously generated content',
-    successText: `Removed previously generated content`,
-    fn: async () => {
-      deleteFile(metadataPath, '.json');
-      deleteFile(collagePath, '.png');
-      deleteFile(rarityJson, '.json');
-      deleteFile(rarityCsv, '.csv');
-    }
-  });
-
   // define metadata collection
   let metadata = [];
 
   // generate artworks and metadata
   const generationsLength = generations.length;
   for (let i = 0; i < generationsLength; i++) {
-    const gen = generations[i];
+    let gen = generations[i];
     const edition = gen.edition.toString();
     const editionOf = `${i+1}/${generationsLength}`;
 
@@ -210,6 +229,11 @@ export default async (basePath: string, opt: any) => {
       });
     }
 
+    // attach rarity to each gen attributes
+    for (const genAttr of gen.attributes) {
+      genAttr.traitRarity = rarity[genAttr.traitType.label][genAttr.traitItem.label];
+    }
+
     // create a single metadata
     const metaPath = pathJoin(metadataPath, edition);
     await task({
@@ -225,6 +249,9 @@ export default async (basePath: string, opt: any) => {
       },
     });
   }
+
+  // write generations with rarity
+  writeJson(generationsPath, generations);
 
   // shuffle metadata collection if required
   const shuffleCount = config.metadata.shuffle;
@@ -263,23 +290,4 @@ export default async (basePath: string, opt: any) => {
       }),
     });
   }
-
-  // populating rarity
-  const rarity = await task({
-    processText: 'Populating rarity',
-    successText: `Collection Rarity is ready`,
-    fn: async () => populateRarity(traits, generations),
-  });
-
-  await task({
-    processText: 'Writing rarity to .json',
-    successText: `Collection Rarity: ${rarityJson}`,
-    fn: async () => writeJson(rarityJson, rarity),
-  });
-
-  await task({
-    processText: 'Writing rarity to .csv',
-    successText: `Collection Rarity: ${rarityCsv}`,
-    fn: async () => rarityToCSV(rarityCsv, rarity),
-  });
 }
